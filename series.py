4a6b608b519445dffe009e19a2528dfbf4935b42
James McConnel <yehoodig@gmail.com>
2019-10-28 22:04:18 -0700

    new script

---
+++ series.py
@@ -0,0 +1,60 @@
+import math
+print("Series type or Maclaurin Expansion function: ")
+series_type=input()
+print(series_type)
+print("n= ")
+i=int(input())
+print("Iterations: ")
+last=int(input())
+
+p=1
+def pseries(p, n):
+	value=1/(n**p)
+	return value
+	
+
+#x=math.pi/2
+x=math.pi/4
+#x=0.5
+def mac_sine(x, n):
+	value=((-1)**n)*((x**(2*n+1))/math.factorial(2*n+1))
+	return value
+	
+a=1
+r=1
+def geometric(a, r, n):
+	value=a*(r**(n-1))
+	return value
+
+if series_type=="p":
+	print("p-value: ")
+	p=eval(input())
+elif series_type=="geometric":
+	print("a-value: ")
+	a=eval(input())
+	print("r-value: ")
+	r=eval(input())
+elif series_type=="sine":
+	print("x-value: ")
+	x=eval(input())
+	print(x)
+
+
+def generator(n):
+	if series_type=="p":
+		return pseries(p,n)
+	if series_type=="geometric":
+		return geometric(a,r,n)
+	if series_type=="sine":
+		return mac_sine(x,n)
+	if series_type=="custom":
+		return custom(n)
+	
+
+sum=0
+while(i<=last):
+	sum=sum+generator(i)
+	print("Term:"+str(i))
+	print(" Value:"+str(generator(i))) 
+	print(" Sum:"+str(sum))
+	i=i+1
