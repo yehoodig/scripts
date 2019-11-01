497c0c4d8f078073f0489b9ab10d928bcc6977ef
James McConnel <yehoodig@gmail.com>
2019-11-01 16:09:36 -0700

    update series

--- series.py
+++ series.py
@@ -20,6 +20,11 @@ def mac_sine(x, n):
 	value=((-1)**n)*((x**(2*n+1))/math.factorial(2*n+1))
 	return value
 	
+
+def mac_cosine(x, n):
+	value=((-1)**n)*((x**(2*n))/math.factorial(2*n))
+	return value
+
 a=1
 r=1
 def geometric(a, r, n):
@@ -34,7 +39,7 @@ elif series_type=="geometric":
 	a=eval(input())
 	print("r-value: ")
 	r=eval(input())
-elif series_type=="sine":
+elif series_type=="sine" or series_type=="cosine" or "tangent":
 	print("x-value: ")
 	x=eval(input())
 	print(x)
@@ -46,15 +51,23 @@ def generator(n):
 	if series_type=="geometric":
 		return geometric(a,r,n)
 	if series_type=="sine":
-		return mac_sine(x,n)
+		return mac_sine(x,n)	
+	if series_type=="cosine":
+		return mac_cosine(x,n)
+	if series_type=="tangent":
+		return (mac_sine(x,n)/mac_cosine(x,n))
 	if series_type=="custom":
 		return custom(n)
 	
+try:
+	sum=0
+	while(i<=last):
+		sum=sum+generator(i)
+		print("Term:"+str(i))
+		print(" Value:"+str(generator(i)))
+		print(" Sum:"+str(sum))
+		i=i+1
+except OverflowError:
+	print("Value or Sum exceeded the limits of the data type")
 
-sum=0
-while(i<=last):
-	sum=sum+generator(i)
-	print("Term:"+str(i))
-	print(" Value:"+str(generator(i))) 
-	print(" Sum:"+str(sum))
-	i=i+1
+	
